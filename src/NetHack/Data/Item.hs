ce3710953839cce3ab6868932500858956d86274
Mikko Juola <mikjuo@gmail.com>
2012-10-20 18:10:14 +0300

    Handle grease in item name canonicalization.

--- src/NetHack/Data/Item.hs
+++ src/NetHack/Data/Item.hs
@@ -21,7 +21,9 @@ data Item = Item ItemStatus String deriving(Show, Eq)
 data ItemStatus = ItemStatus { quantity :: Maybe Int,
                                buc :: Maybe BUC,
                                enchantment :: Maybe Int,
-                               charging :: Maybe (Int, Int) }
+                               charging :: Maybe (Int, Int),
+                               greased :: Bool,
+                               naming :: Maybe String }
                              deriving(Show, Eq)
 
 item :: ItemStatus -> String -> Item
@@ -56,6 +58,7 @@ canonicalizeItemName str =
   where
     str2 = (removeIndefiniteArticles . trim $ str)
 
+-- TODO: refactor
 canonicalize :: String -> (ItemStatus, String)
 canonicalize str =
   case match "^([0-9]+ )(.+)" str :: Maybe (String, String) of
@@ -67,40 +70,61 @@ canonicalize str =
     case (match "^(blessed|uncursed|cursed) (.+)" str) :: [String] of
       [] -> canonicalize3 quantity Nothing str
       [buc,rest] -> canonicalize3 quantity (Just buc) rest
+
   canonicalize3 quantity buc str =
+    case match "^greased (.+)" str :: [String] of
+      [] -> canonicalize4 quantity buc False str
+      [rest] -> canonicalize4 quantity buc True rest
+
+  canonicalize4 quantity buc greased str =
     case (match ("^(corroded|very corroded|thoroughly corroded|" ++
                "rusty|very rusty|thoroughly rusty|" ++
                "rotted|very rotted|thoroughly rotted|" ++
                "burnt|very burnt|thoroughly burnt|" ++
                "fixed|rotproof|fireproof|rustproof) (.+)") str) :: [String]
                of
-      [] -> canonicalize4 quantity buc str
-      [_,rest] -> canonicalize4 quantity buc rest
-  canonicalize4 quantity buc str =
+      [] -> canonicalize5 quantity buc greased str
+      [_,rest] -> canonicalize5 quantity buc greased rest
+
+  canonicalize5 quantity buc greased str =
     case (match "^((\\+|-)[0-9]+ )(.+)" str) :: [String] of
-      []                        -> canonicalize5 quantity buc Nothing str
-      [enchantment, _, rest] -> canonicalize5 quantity buc
+      []                        -> canonicalize6 quantity buc greased
+                                          Nothing str
+      [enchantment, _, rest] -> canonicalize6 quantity buc greased
                                           (Just enchantment) rest
 
-  canonicalize5 quantity buc enchantment str =
+  canonicalize6 quantity buc greased enchantment str =
     case (match "^(.+) \\(((\\+|-)?([0-9]+)):(((\\+|-)?[0-9]+))\\)" str) :: [String] of
-      [] -> canonicalize6 quantity buc enchantment Nothing str
+      [] -> canonicalize7 quantity buc greased enchantment Nothing str
       [rest, charged, _, _, charges, _, _] ->
-        canonicalize6 quantity buc enchantment (Just (charged, charges)) rest
+        canonicalize7 quantity buc greased enchantment
+           (Just (charged, charges)) rest
 
-  canonicalize6 quantity buc enchantment charging str =
+  canonicalize7 quantity buc greased enchantment charging str =
+    case match ("^(.+) (\\(weapon in .*\\)|" ++
+               "\\(alternate weapon; not wielded\\)|" ++
+               "\\(being worn\\)|" ++
+               "\\(in quiver\\)|" ++
+               "\\(lit\\)|" ++
+               "\\(embedded in .*\\))") str :: [String] of
+      [] -> canonicalize8 quantity buc greased enchantment charging str
+      (rest:_) -> canonicalize8 quantity buc greased enchantment charging rest
+
+  canonicalize8 quantity buc greased enchantment charging str =
+    case match "^(.+) named (.+)$" str :: [String] of
+      [] -> canonicalize9 quantity buc greased enchantment charging Nothing str
+      [rest,naming] ->
+        canonicalize9 quantity buc greased enchantment charging
+           (Just naming) rest
+
+  canonicalize9 quantity buc greased enchantment charging naming str =
     (ItemStatus { quantity = fmap convertQuantity quantity,
                   enchantment = fmap convertEnchantment enchantment,
                   buc = fmap convertBUC buc,
-                  charging = fmap convertCharging charging },
-      case match ("^(.+) (\\(weapon in .*\\)|" ++
-                 "\\(alternate weapon; not wielded\\)|" ++
-                 "\\(being worn\\)|" ++
-                 "\\(in quiver\\)|" ++
-                 "\\(lit\\)|" ++
-                 "\\(embedded in .*\\))") str :: [String] of
-        [] -> str
-        (rest:_) -> rest)
+                  greased = greased,
+                  charging = fmap convertCharging charging,
+                  naming = naming },
+      str)
 
 removeIndefiniteArticles :: String -> String
 removeIndefiniteArticles ('a':' ':rest) = rest
